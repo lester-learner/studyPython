bf6db2d4f90c9b6ba5a5614ee3c97eca6f2bb1cc
lester-learner <wang91735@gmail,com>
2020-08-09 19:35:55 +0800

    'test2'

--- first.py
+++ first.py
@@ -1,4 +1,11 @@
-if (n := len('a')):
-    print(f"List is too long ({n} elements, expected <= 10)")
-else:
-    print(n)
+class Person:
+    def who(self):
+        print(self)
+
+        
+zhangsan = Person()
+#第一种方式
+zhangsan.who()
+#第二种方式
+who = zhangsan.who
+who()#通过 who 变量调用zhangsan对象中的 who() 方法
